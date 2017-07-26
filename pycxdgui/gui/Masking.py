GUI: some masking issues fixed

pycxdgui/gui/Masking.py
@@ -14,7 +14,7 @@ from classes.Mask import Mask
 from gui.guitools import findLowHigh
 
 class MPoly(QtGui.QMainWindow):
-    def __init__(self, img, datatable=None, mask=None):
+    def __init__(self, img, datatable=None, mask=None, imgwidget=None):
         ''' Initialize the polygon for mask.
                 This object handles the graphics as well as the mask
                 creation (it might be a good idea to separate them later
@@ -33,6 +33,9 @@ class MPoly(QtGui.QMainWindow):
         # TODO : remove and add error checking
         if img is None:
             img = np.ones((100,100))
+        # pointer to imgwidget
+        self.imgwidget = imgwidget
+
         self.blemish = np.ones_like(img)
         self.setimgdata(img.astype(float))
         self.points = []
@@ -354,12 +357,12 @@ class MPoly(QtGui.QMainWindow):
         self.mimg.setImage(self.mask,autoLevels=False,levels=[0,2])
 
     def sendMask(self):
-        #self.xDset.mask = self.mask
-        pass
+        if self.imgwidget is not None:
+            self.imgwidget.mask = self._mask.mask
+        else:
+            print("Error, don't have a reference to image window, cannot send")
 
     def callExit(self):
-        #self.xDset.mask = self.mask
-        pass
         self.close()
 
     def mouseClickEvent(self,ev):

pycxdgui/gui/SAXSMainGUI.py
@@ -11,7 +11,7 @@ from tools.qphiavg import qphiavg
 
 from skbeam.core.roi import circular_average
 
-from tools.mask import openmask
+import tools.mask
 
 from gui.SAXSWidget import SAXSWidget
 from gui.Masking import MPoly
@@ -193,7 +193,7 @@ class SAXSGUI(QtGui.QMainWindow):
         # TODO : make general fucntion
         if self.avg_img is None:
             self.avg_img = np.ones((100,100))
-        self.maskprog = MPoly(self.avg_img.astype(float),mask=None)
+        self.maskprog = MPoly(self.avg_img.astype(float),mask=None, imgwidget=self.imgwidget)
 
     def circavg(self):
         self.imgwidget.circavg()
@@ -246,7 +246,7 @@ class SAXSGUI(QtGui.QMainWindow):
             self.imgwidget.redrawimg()
 
     def openmaskfile(self):
-        fname = QtGui.QFileDialog.getOpenFileName(self, 'Open Mask', self.getSDIR(),
+        fname, filetype = QtGui.QFileDialog.getOpenFileName(self, 'Open Mask', self.getSDIR(),
                             "Masks (*mask*.hd5 *tif *tiff);;Blemish Files (*blemish*.hd5);; All Files (*)")
         self.loadmaskfromfile(fname)
 
@@ -331,7 +331,7 @@ class SAXSGUI(QtGui.QMainWindow):
             mask_threshold = int(self.saxsdata.getelem("setup", "mask_threshold"))
             if mask_name.endswith("h5") or mask_name.endswith("hd5"):
                 # premask is mask, and mask is thresholded (useful when using a flatfield)
-                self.premask = openmask(mask_name)
+                self.premask = tools.mask.openmask(mask_name)
             elif mask_name.lower().endswith("tif") or mask_name.lower().endswith("tiff"):
                 # save two instances of mask
                 self.premask = np.array(Image.open(mask_name))
@@ -344,10 +344,15 @@ class SAXSGUI(QtGui.QMainWindow):
                 self.mask = (self.premask >= mask_threshold).astype(int)
             else:
                 self.mask = None
+            if hasattr(self, 'imgwidget'):
+                self.imgwidget.mask = self.mask
+
 
     def average_frames(self):
         if self.imgreader is not None:
             self.avg_img, self.Ivst = runningaverage(self.imgreader)
+            if self.mask is not None:
+                self.avg_img = self.avg_img
         else:
             print("Cannot average images, no images loaded")
 